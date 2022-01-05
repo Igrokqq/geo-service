49bc2dd4a6da1abff166ea88f292dae3b9166f7a
Vadim Filipovsky <vadim.filipovsky@gmail.com>
2022-01-05 22:35:20 +0100

    feat: timezone handler

--- internal/application/timezone.go
+++ internal/application/timezone.go
@@ -10,6 +10,16 @@ type TimezoneService struct {
 	r timezone.Repository
 }
 
+func (th *TimezoneService) Get(id uint) (*timezone.Timezone, error) {
+	tz, err := th.r.Find(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tz, nil
+}
+
 func NewTimezoneService(db *gorm.DB) *TimezoneService {
 	return &TimezoneService{
 		r: persistance.NewTimezoneRepository(db),
