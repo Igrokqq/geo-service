4c2ad598d33dd92de666ba8931f8e6761d00c335
Vadim Filipovsky <vadim.filipovsky@gmail.com>
2022-01-06 14:33:32 +0100

    wip: working on handlers. need to create a separate parser and validator for request

--- internal/api/v1/timezone.go
+++ internal/api/v1/timezone.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/vfilipovsky/geo-service/internal/api"
@@ -30,26 +31,57 @@ func AddTimezoneRoutes(r *mux.Router, db *gorm.DB) {
 	r.HandleFunc(api.V1Timezone+"/{id}", h.Delete).Methods(http.MethodDelete)
 }
 
-func (h *TimezoneHandler) List(w http.ResponseWriter, r *http.Request) {
-	api.Respond(w, "not implemented yet", http.StatusNotImplemented)
+func (h *TimezoneHandler) List(w http.ResponseWriter, _ *http.Request) {
+	list, err := h.s.List()
+
+	if err != nil {
+		api.Respond(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	api.Respond(w, &list, http.StatusOK)
 }
 
 func (h *TimezoneHandler) Delete(w http.ResponseWriter, r *http.Request) {
-	api.Respond(w, "not implemented yet", http.StatusNotImplemented)
+	var err error
+
+	// todo need request parser and validator with human read messages
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	i, err := strconv.ParseUint(id, 10, 64)
+
+	if err != nil {
+		api.Respond(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := h.s.Delete(uint(i)); err != nil {
+		api.Respond(w, err.Error(), http.StatusOK)
+		return
+	}
+
+	api.Respond(w, "", http.StatusOK)
 }
 
 func (h *TimezoneHandler) Update(w http.ResponseWriter, r *http.Request) {
-	api.Respond(w, "not implemented yet", http.StatusNotImplemented)
+	//todo: parse and validate request
+	//todo: create dto
+	api.Respond(w, "", http.StatusNotImplemented)
 }
 
 func (h *TimezoneHandler) Create(w http.ResponseWriter, r *http.Request) {
-	api.Respond(w, "not implemented yet", http.StatusNotImplemented)
+	//todo: parse and validate request
+	//todo: create dto
+	api.Respond(w, "", http.StatusNotImplemented)
 }
 
 func (h *TimezoneHandler) GetById(w http.ResponseWriter, r *http.Request) {
-	api.Respond(w, "not implemented yet", http.StatusNotImplemented)
+	//todo: parse and validate request
+	api.Respond(w, "", http.StatusNotImplemented)
 }
 
 func (h *TimezoneHandler) GetByName(w http.ResponseWriter, r *http.Request) {
-	api.Respond(w, "not implemented yet", http.StatusNotImplemented)
+	//todo: parse and validate request
+	api.Respond(w, "", http.StatusNotImplemented)
 }
