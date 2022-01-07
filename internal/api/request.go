d81f0e4e117ee5d9ad50598afd9f729a111e3f04
Vadim Filipovsky <vadim.filipovsky@gmail.com>
2022-01-07 16:30:47 +0100

    feat: parse to uint function

--- internal/api/request.go
+++ internal/api/request.go
@@ -1,3 +1,19 @@
 package api
 
-// todo: create a function to validate api request and parse it into prepared payload/dto
+import (
+	"strconv"
+
+	"github.com/go-playground/validator"
+)
+
+var Validate = validator.New()
+
+func ParseIdToUint(id string) (uint, error) {
+	i, err := strconv.Atoi(id)
+
+	if err != nil {
+		return 0, ErrIdMustBeAPositiveNum
+	}
+
+	return uint(i), nil
+}
